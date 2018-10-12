ff164f7a193d5c8d34b708a664ee30356c632430
Calvin Koepke <hello@calvinkoepke.com>
2018-10-11 21:44:16 -0600

    restructure starter files

--- scripts/build-theme.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if ( ! function_exists( 'shell_exec' ) ) {
-	echo 'Your environment does not support executing shell scripts from PHP.';
-	die;
-}
-
-shell_exec( __DIR__ . '/bin/bash /scripts/shell/functions.sh ' . get_stylesheet_directory() );
