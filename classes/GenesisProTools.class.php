b60b6b845972ec73a7db1e251fc609aa9a41665f
Calvin Koepke <hello@calvinkoepke.com>
2018-11-12 21:50:38 -0700

    fix CHILD_THEME_HANDLE

--- classes/GenesisProTools.class.php
+++ classes/GenesisProTools.class.php
@@ -44,29 +44,29 @@ final class GenesisProTools {
 		$theme = \wp_get_theme();
 
 		// Core paths.
-		define( 'GPT_CORE_PATH',       \plugin_dir_path( __DIR__ )               );
-		define( 'GPT_CORE_URL',        \plugin_dir_url( __DIR__ )                );
-		define( 'CHILD_ROOT_PATH',     \get_stylesheet_directory()               );
-		define( 'CHILD_ROOT_URL',      \get_stylesheet_directory_uri()           );
-		define( 'CHILD_VENDOR_PATH',   CHILD_ROOT_PATH . '/vendor'               );
-		define( 'CHILD_VENDOR_URL',    CHILD_ROOT_URL . '/vendor'                );
+		define( 'GPT_CORE_PATH',       \plugin_dir_path( __DIR__ )                    );
+		define( 'GPT_CORE_URL',        \plugin_dir_url( __DIR__ )                     );
+		define( 'CHILD_ROOT_PATH',     \get_stylesheet_directory()                    );
+		define( 'CHILD_ROOT_URL',      \get_stylesheet_directory_uri()                );
+		define( 'CHILD_VENDOR_PATH',   CHILD_ROOT_PATH . '/vendor'                    );
+		define( 'CHILD_VENDOR_URL',    CHILD_ROOT_URL . '/vendor'                     );
 
 		// Asset paths.
-		define( 'CHILD_CSS_PATH',      CHILD_ROOT_PATH . '/dist/css'             );
-		define( 'CHILD_CSS_URL',       CHILD_ROOT_URL . '/dist/css'              );
-		define( 'CHILD_JS_PATH',       CHILD_ROOT_PATH . '/dist/js'              );
-		define( 'CHILD_JS_URL',        CHILD_ROOT_URL . '/dist/js'               );
-		define( 'CHILD_IMAGE_PATH',    CHILD_ROOT_PATH . '/dist/images'          );
-		define( 'CHILD_IMAGE_URL',     CHILD_ROOT_URL . '/dist/images'           );
-		define( 'CHILD_SVG_PATH',      CHILD_ROOT_PATH . '/dist/svg'             );
-		define( 'CHILD_SVG_URL',       CHILD_ROOT_URL . '/dist/svg'              );
+		define( 'CHILD_CSS_PATH',      CHILD_ROOT_PATH . '/dist/css'                  );
+		define( 'CHILD_CSS_URL',       CHILD_ROOT_URL . '/dist/css'                   );
+		define( 'CHILD_JS_PATH',       CHILD_ROOT_PATH . '/dist/js'                   );
+		define( 'CHILD_JS_URL',        CHILD_ROOT_URL . '/dist/js'                    );
+		define( 'CHILD_IMAGE_PATH',    CHILD_ROOT_PATH . '/dist/images'               );
+		define( 'CHILD_IMAGE_URL',     CHILD_ROOT_URL . '/dist/images'                );
+		define( 'CHILD_SVG_PATH',      CHILD_ROOT_PATH . '/dist/svg'                  );
+		define( 'CHILD_SVG_URL',       CHILD_ROOT_URL . '/dist/svg'                   );
 
 		// Theme information.
-		define( 'CHILD_THEME_NAME',    $theme->get('Name')                       );
-		define( 'CHILD_THEME_URL',     $theme->get('ThemeURI')                   );
-		define( 'CHILD_THEME_VERSION', $theme->get('Version')                    );
-		define( 'CHILD_THEME_AUTHORS', $theme->get('Author')                     );
-		define( 'CHILD_THEME_HANDLE',  strtolower( CHILD_THEME_NAME )            );
+		define( 'CHILD_THEME_NAME',    $theme->get('Name')                            );
+		define( 'CHILD_THEME_URL',     $theme->get('ThemeURI')                        );
+		define( 'CHILD_THEME_VERSION', $theme->get('Version')                         );
+		define( 'CHILD_THEME_AUTHORS', $theme->get('Author')                          );
+		define( 'CHILD_THEME_HANDLE',  sanitize_title_with_dashes( CHILD_THEME_NAME ) );
 
 		// Ensure Genesis is loaded first.
 		add_action( 'after_setup_theme', function() {
